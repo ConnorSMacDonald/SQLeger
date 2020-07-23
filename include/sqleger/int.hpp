d0f2ecd002a9c956751586edb4a9368591802763
csmacdonald95 <csmacdonald95@gmail.com>
2020-07-23 19:32:59 -0400

    Add int.hpp

---
+++ include/sqleger/int.hpp
@@ -0,0 +1,17 @@
+#ifndef SQLEGER_INT_HPP_INCLUDED
+#define SQLEGER_INT_HPP_INCLUDED
+
+#include <sqlite3.h>
+
+
+namespace sqleger {
+
+
+using int64 = ::sqlite3_int64;
+using uint64 = ::sqlite3_uint64;
+
+
+}; // namespace sqleger
+
+
+#endif
