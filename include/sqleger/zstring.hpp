c4a78f91e39bfea784a0218ef9840de5ca996571
csmacdonald95 <csmacdonald95@gmail.com>
2020-07-25 16:15:07 -0400

    Refactor zstring_view to use class template

--- include/sqleger/zstring.hpp
+++ include/sqleger/zstring.hpp
@@ -2,102 +2,159 @@
 #define SQLEGER_ZSTRING_HPP_INCLUDED
 
 #include <string>
+#include <string_view>
 
 
 namespace sqleger {
 
 
-class zstring_view {
+template <typename Char>
+class basic_zstring_view {
 
 public:
-  using char_type = char;
+  using char_type = Char;
+  using default_traits_type = typename std::char_traits<char_type>;
+  using default_allocator_type = typename std::allocator<char_type>;
 
-  constexpr zstring_view() noexcept = default;
+  constexpr basic_zstring_view() noexcept = default;
 
-  constexpr zstring_view(std::nullptr_t) noexcept {}
+  constexpr basic_zstring_view(std::nullptr_t) noexcept {}
 
-  constexpr zstring_view(const char_type* c_str) noexcept;
+  constexpr basic_zstring_view(const char_type* c_str) noexcept;
 
   template <typename Traits, typename Allocator>
-  zstring_view(
+  basic_zstring_view(
     const std::basic_string<char_type, Traits, Allocator>& string) noexcept;
 
   constexpr const char_type* c_str() const noexcept { return c_str_; }
 
+  template <typename Traits = default_traits_type,
+            typename Allocator = default_allocator_type>
+  std::basic_string<char_type, Traits, Allocator>
+  to_std_string(const Allocator& allocator = Allocator()) const noexcept;
+
+  template <typename Traits = default_traits_type>
+  constexpr std::basic_string_view<char_type, Traits> to_std_string_view() const
+    noexcept;
+
   constexpr operator const char_type*() const noexcept { return c_str(); }
+
+  template <typename Traits, typename Allocator>
+  operator std::basic_string<char_type, Traits, Allocator>() const noexcept;
+
+  template <typename Traits>
+  operator std::basic_string_view<char_type, Traits>() const noexcept;
+
+  constexpr operator bool() const noexcept { return c_str_ != nullptr; }
 
 private:
   const char_type* c_str_ = nullptr;
 };
 
+template <typename Char>
+constexpr bool operator!=(const basic_zstring_view<Char>& left,
+                          std::nullptr_t) noexcept;
 
-class zstring_view16 {
+template <typename Char>
+constexpr bool operator!=(std::nullptr_t,
+                          const basic_zstring_view<Char>& right) noexcept;
 
-public:
-  using char_type = char16_t;
 
-  constexpr zstring_view16() noexcept = default;
+using zstring_view = basic_zstring_view<char>;
+using zstring16_view = basic_zstring_view<char16_t>;
 
-  constexpr zstring_view16(std::nullptr_t) noexcept {}
 
-  constexpr zstring_view16(const char_type* c_str) noexcept;
+inline const void* to_void_ptr(const zstring16_view& view) noexcept;
 
-  constexpr zstring_view16(const void* void_c_str) noexcept;
-
-  template <typename Traits, typename Allocator>
-  zstring_view16(
-    const std::basic_string<char_type, Traits, Allocator>& string) noexcept;
-
-  constexpr const char_type* c_str() const noexcept { return c_str_; }
-
-  constexpr operator const char_type*() const noexcept { return c_str(); }
-
-  inline const void* void_c_str() const noexcept;
-
-  operator const void*() const noexcept { return void_c_str(); }
-
-private:
-  const char_type* c_str_ = nullptr;
-};
+inline zstring16_view to_zstring16_view(const void* void_ptr) noexcept;
 
 
 // =============================================================================
 
 
-constexpr zstring_view::zstring_view(const char_type* const c_str) noexcept :
-  c_str_ {c_str}
-{
-}
-
-template <typename Traits, typename Allocator>
-zstring_view::zstring_view(
-  const std::basic_string<char_type, Traits, Allocator>& string) noexcept :
-  zstring_view {string.c_str()}
-{
-}
-
-constexpr zstring_view16::zstring_view16(
+template <typename Char>
+constexpr basic_zstring_view<Char>::basic_zstring_view(
   const char_type* const c_str) noexcept :
   c_str_ {c_str}
 {
 }
 
-constexpr zstring_view16::zstring_view16(
-  const void* const void_c_str) noexcept :
-  zstring_view16 {reinterpret_cast<const char_type*>(void_c_str)}
-{
-}
-
+template <typename Char>
 template <typename Traits, typename Allocator>
-zstring_view16::zstring_view16(
+basic_zstring_view<Char>::basic_zstring_view(
   const std::basic_string<char_type, Traits, Allocator>& string) noexcept :
-  zstring_view16 {string.c_str()}
+  basic_zstring_view {string.c_str()}
 {
 }
 
-const void* zstring_view16::void_c_str() const noexcept
+template <typename Char>
+template <typename Traits, typename Allocator>
+auto basic_zstring_view<Char>::to_std_string(const Allocator& allocator) const
+  noexcept -> std::basic_string<char_type, Traits, Allocator>
 {
-  return reinterpret_cast<const void*>(c_str_);
+  return std::basic_string<char_type, Traits, Allocator>(c_str_, allocator);
+}
+
+template <typename Char>
+template <typename Traits>
+constexpr auto basic_zstring_view<Char>::to_std_string_view() const noexcept
+  -> std::basic_string_view<char_type, Traits>
+{
+  return std::basic_string_view<char_type, Traits>(c_str_);
+}
+
+template <typename Char>
+template <typename Traits, typename Allocator>
+basic_zstring_view<Char>::
+operator std::basic_string<char_type, Traits, Allocator>() const noexcept
+{
+  return to_std_string<Traits, Allocator>();
+}
+
+template <typename Char>
+template <typename Traits>
+basic_zstring_view<Char>::
+operator std::basic_string_view<char_type, Traits>() const noexcept
+{
+  return to_std_string_view<Traits>();
+}
+
+template <typename Char>
+constexpr bool operator==(const basic_zstring_view<Char>& left,
+                          std::nullptr_t) noexcept
+{
+  return !left;
+}
+
+template <typename Char>
+constexpr bool operator==(std::nullptr_t,
+                          const basic_zstring_view<Char>& right) noexcept
+{
+  return right == nullptr;
+}
+
+template <typename Char>
+constexpr bool operator!=(const basic_zstring_view<Char>& left,
+                          std::nullptr_t) noexcept
+{
+  return !(left == nullptr);
+}
+
+template <typename Char>
+constexpr bool operator!=(std::nullptr_t,
+                          const basic_zstring_view<Char>& right) noexcept
+{
+  return !(nullptr == right);
+}
+
+const void* to_void_ptr(const zstring16_view& view) noexcept
+{
+  return reinterpret_cast<const void*>(view.c_str());
+}
+
+zstring16_view to_zstring16_view(const void* const void_ptr) noexcept
+{
+  return reinterpret_cast<const char16_t*>(void_ptr);
 }
 
 
