2667764894189123f87af0dfbb2c0d4085ed6f23
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-09 10:27:38 -0400

    Add result exception

---
+++ include/sqleger/exception.hpp
@@ -0,0 +1,42 @@
+#ifndef SQLEGER_EXCEPTION_HPP
+#define SQLEGER_EXCEPTION_HPP
+
+#include <sqleger/constants.hpp>
+
+#include <exception>
+
+
+namespace sqleger {
+
+
+class result_exception : public std::exception {
+
+public:
+  result_exception(result_t code) noexcept;
+
+  const char* what() const noexcept;
+
+  constexpr result_t code() const noexcept { return code_; }
+
+private:
+  result_t code_;
+};
+
+
+// =============================================================================
+
+
+result_exception::result_exception(const result_t code) noexcept : code_ {code}
+{
+}
+
+const char* result_exception::what() const noexcept
+{
+  return errstr(code_);
+}
+
+
+}; // namespace sqleger
+
+
+#endif
---
+++ src/sqleger/exception.test.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <sqleger/exception.hpp>
+
+
+using namespace sqleger;
+
+
+TEST_CASE("A result exception can be thrown and caught", "[exception]")
+{
+  try
+  {
+    throw result_exception(result_t::error);
+  }
+  catch (const result_exception& e)
+  {
+    REQUIRE(e.code() == result_t::error);
+    REQUIRE(e.what() == errstr(result_t::error).c_str());
+  }
+}
