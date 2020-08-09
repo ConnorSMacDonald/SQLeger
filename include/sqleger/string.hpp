c33c6277b63fc48c91e671d07fa6d9a7af1ca798
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-09 18:13:46 -0400

    Add string span

--- include/sqleger/string.hpp
+++ include/sqleger/string.hpp
@@ -55,6 +55,55 @@ const Char** to_c_str_ptr(basic_zstring_view<Char>& view) noexcept;
 using zstring_view = basic_zstring_view<char>;
 
 
+template <typename Char>
+class basic_string_span {
+
+public:
+  using char_type = Char;
+  using default_traits_type = typename std::char_traits<char_type>;
+  using default_allocator_type = typename std::allocator<char_type>;
+  using size_type = int;
+
+  static constexpr size_type zstring_size = -1;
+
+  constexpr basic_string_span(const char_type* data,
+                              size_type size = zstring_size) noexcept;
+
+  template <typename Traits, typename Allocator>
+  basic_string_span(
+    const std::basic_string<char_type, Traits, Allocator>& string) noexcept;
+
+  template <typename Traits>
+  constexpr basic_string_span(
+    const std::basic_string_view<char_type, Traits>& string_view) noexcept;
+
+  template <typename Traits = default_traits_type,
+            typename Allocator = default_allocator_type>
+  std::basic_string<char_type, Traits, Allocator>
+  to_std_string(const Allocator& allocator = Allocator()) const;
+
+  template <typename Traits = default_traits_type>
+  constexpr std::basic_string_view<char_type, Traits> to_std_string_view() const
+    noexcept;
+
+  template <typename Traits, typename Allocator>
+  operator std::basic_string<char_type, Traits, Allocator>() const;
+
+  template <typename Traits>
+  constexpr operator std::basic_string_view<char_type, Traits>() const noexcept;
+
+  constexpr const char_type* data() const noexcept { return data_; }
+
+  constexpr size_type size() const noexcept { return size_; }
+
+private:
+  const char_type* data_ = nullptr;
+  size_type size_ = zstring_size;
+};
+
+using string_span = basic_string_span<char>;
+
+
 // =============================================================================
 
 
@@ -115,6 +164,74 @@ template <typename Char>
 const Char** to_c_str_ptr(basic_zstring_view<Char>& view) noexcept
 {
   return to_c_str_ptr(&view);
+}
+
+template <typename Char>
+constexpr basic_string_span<Char>::basic_string_span(
+  const char_type* const data,
+  const size_type size) noexcept :
+  data_ {data},
+  size_ {size}
+{
+}
+
+template <typename Char>
+template <typename Traits, typename Allocator>
+basic_string_span<Char>::basic_string_span(
+  const std::basic_string<char_type, Traits, Allocator>& string) noexcept :
+  basic_string_span {string.c_str(), static_cast<size_type>(string.size())}
+{
+}
+
+template <typename Char>
+template <typename Traits>
+constexpr basic_string_span<Char>::basic_string_span(
+  const std::basic_string_view<char_type, Traits>& string_view) noexcept :
+  basic_string_span {string_view.data(),
+                     static_cast<size_type>(string_view.size())}
+{
+}
+
+template <typename Char>
+template <typename Traits, typename Allocator>
+auto basic_string_span<Char>::to_std_string(const Allocator& allocator) const
+  -> std::basic_string<char_type, Traits, Allocator>
+{
+  using result_type = std::basic_string<char_type, Traits, Allocator>;
+
+  return (size_ == zstring_size)
+           ? result_type(data_)
+           : result_type(data_,
+                         static_cast<typename result_type::size_type>(size_));
+}
+
+template <typename Char>
+template <typename Traits>
+constexpr auto basic_string_span<Char>::to_std_string_view() const noexcept
+  -> std::basic_string_view<char_type, Traits>
+{
+  using result_type = std::basic_string_view<char_type, Traits>;
+
+  return (size_ == zstring_size)
+           ? result_type(data_)
+           : result_type(data_,
+                         static_cast<typename result_type::size_type>(size_));
+}
+
+template <typename Char>
+template <typename Traits, typename Allocator>
+basic_string_span<Char>::
+operator std::basic_string<char_type, Traits, Allocator>() const
+{
+  return to_std_string();
+}
+
+template <typename Char>
+template <typename Traits>
+constexpr basic_string_span<Char>::
+operator std::basic_string_view<char_type, Traits>() const noexcept
+{
+  return to_std_string_view();
 }
 
 
--- src/sqleger/string.test.cpp
+++ src/sqleger/string.test.cpp
@@ -3,8 +3,13 @@
 
 #include <sqleger/string.hpp>
 
+#include <cstring>
+
 
 using namespace sqleger;
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
 
 
 TEST_CASE("A zstring view can be constructed from a C-style string", "[string]")
@@ -18,10 +23,10 @@ TEST_CASE("A zstring view can be constructed from a C-style string", "[string]")
 TEST_CASE("A zstring view can be constructed from a standard string",
           "[string]")
 {
-  const auto ss = std::string("std::string");
-  const auto zv = zstring_view(ss);
+  const auto stds = std::string("std::string");
+  const auto zv = zstring_view(stds);
 
-  REQUIRE(zv.c_str() == ss.c_str());
+  REQUIRE(zv.c_str() == stds.c_str());
 }
 
 TEST_CASE("A zstring view can be converted to a C-style string", "[string]")
@@ -35,18 +40,18 @@ TEST_CASE("A zstring view can be converted to a C-style string", "[string]")
 TEST_CASE("A zstring view can be converted to a standard string", "[string]")
 {
   constexpr auto zv = zstring_view("null terminated strings are a hack");
-  const auto ss = static_cast<std::string>(zv);
+  const auto stds = static_cast<std::string>(zv);
 
-  REQUIRE(ss == zv.c_str());
+  REQUIRE(stds == zv.c_str());
 }
 
 TEST_CASE("A zstring view can be converted to a standard string view",
           "[string]")
 {
   constexpr auto zv = zstring_view("I can't wait to be a std::string_view");
-  constexpr auto ssv = static_cast<std::string_view>(zv);
+  constexpr auto stdsv = static_cast<std::string_view>(zv);
 
-  REQUIRE(ssv.data() == zv.c_str());
+  REQUIRE(stdsv.data() == zv.c_str());
 }
 
 TEST_CASE("A zstring view reference or pointer can converted to a pointer to a "
@@ -62,4 +67,89 @@ TEST_CASE("A zstring view reference or pointer can converted to a pointer to a "
   const auto** const cs2 = to_c_str_ptr(&zv);
 
   REQUIRE(*cs2 == zv.c_str());
+}
+
+TEST_CASE("A string span can be constructed from a C-style", "[string]")
+{
+  constexpr auto* const cs = "foo bar baz something something";
+  const auto ss = string_span(cs);
+
+  REQUIRE(ss.size() == string_span::zstring_size);
+  REQUIRE(ss.data() == cs);
+}
+
+TEST_CASE("A string span can be constructed from a char pointer and a size",
+          "[string]")
+{
+  constexpr auto* const cs = "ihg avsa sa sa ssasAS da dfasdfasd a";
+  const auto ss = string_span(cs, 7);
+
+  REQUIRE(ss.size() == 7);
+  REQUIRE(ss.data() == cs);
+}
+
+TEST_CASE("A string span can be constructed from a standard string", "[string]")
+{
+  const auto stds = "jljj asjkd fjklasdfj sajl;ASDF ASD"s;
+  const auto ss = string_span(stds);
+
+  REQUIRE(ss.size() == static_cast<string_span::size_type>(stds.size()));
+  REQUIRE(ss.data() == stds.c_str());
+}
+
+TEST_CASE("A string span can be constructed from a standard string view",
+          "[string]")
+{
+  const auto stdsv = "jlasvjklsvjksv jalksdv jvdsajdsva;jdls"sv;
+  const auto ss = string_span(stdsv);
+
+  REQUIRE(ss.size() == static_cast<string_span::size_type>(stdsv.size()));
+  REQUIRE(ss.data() == stdsv.data());
+}
+
+TEST_CASE("A string span can be converted to standard string", "[string]")
+{
+  SECTION("null-terminated")
+  {
+    constexpr auto* const cs = "asdlvasdf jasd dasfjasdf0000";
+    const auto ss = string_span(cs);
+    const auto stds = static_cast<std::string>(ss);
+
+    REQUIRE(stds.size() == std::strlen(cs));
+    REQUIRE(stds == ss.data());
+  }
+
+  SECTION("not null-terminated")
+  {
+    const auto size = 4;
+    const auto ss = string_span("asdlvasdf jasd dasfjasdf0000", size);
+    const auto stds = static_cast<std::string>(ss);
+
+    REQUIRE(stds.size() == size);
+    REQUIRE(stds == std::string_view(ss.data(), size));
+  }
+}
+
+TEST_CASE("A string span can be converted to a standard string view",
+          "[string]")
+{
+  SECTION("null-terminated")
+  {
+    constexpr auto* const cs = "asdlvasdf jasd dasfjasdf0000";
+    const auto ss = string_span(cs);
+    const auto stdsv = static_cast<std::string_view>(ss);
+
+    REQUIRE(stdsv.size() == std::strlen(cs));
+    REQUIRE(stdsv.data() == ss.data());
+  }
+
+  SECTION("not null-terminated")
+  {
+    const auto size = 4;
+    const auto ss = string_span("asdlvasdf jasd dasfjasdf0000", size);
+    const auto stdsv = static_cast<std::string_view>(ss);
+
+    REQUIRE(stdsv.size() == size);
+    REQUIRE(stdsv.data() == ss.data());
+  }
 }
