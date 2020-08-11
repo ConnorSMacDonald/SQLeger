25345af134fe4b86f368e23edaa3f39dad190ebe
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-10 22:15:14 -0400

    Update main header

--- include/sqleger/sqleger.hpp
+++ include/sqleger/sqleger.hpp
@@ -1,16 +1,14 @@
-#ifndef SQLEGER_SQLEGER_HPP_INCLUDED
-#define SQLEGER_SQLEGER_HPP_INCLUDED
+#ifndef SQLEGER_SQLEGER_HPP
+#define SQLEGER_SQLEGER_HPP
 
-#include <sqlite3.h>
+#include <sqleger/db_decl.hpp>
 
+#include <sqleger/constants.hpp>
+#include <sqleger/exception.hpp>
+#include <sqleger/int.hpp>
+#include <sqleger/stmt.hpp>
+#include <sqleger/string.hpp>
 
-namespace sqleger {
-
-
-
-
-
-};
-
+#include <sqleger/db.hpp>
 
 #endif
