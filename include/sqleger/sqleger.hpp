28f5a6bc59325b0128ea4c18a92b0994461c38b7
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-15 16:24:38 -0400

    Update main header

--- include/sqleger/sqleger.hpp
+++ include/sqleger/sqleger.hpp
@@ -4,10 +4,14 @@
 #include <sqleger/db_decl.hpp>
 
 #include <sqleger/constants.hpp>
+#include <sqleger/destructor.hpp>
 #include <sqleger/int.hpp>
 #include <sqleger/result_exception.hpp>
 #include <sqleger/stmt.hpp>
 #include <sqleger/string.hpp>
+#include <sqleger/user_blob.hpp>
+#include <sqleger/user_text.hpp>
+#include <sqleger/utility.hpp>
 
 #include <sqleger/db.hpp>
 
