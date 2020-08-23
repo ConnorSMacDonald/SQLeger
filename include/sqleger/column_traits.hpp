e60942dce3eb8011ea79631b01855c8f9e8ce3df
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-23 11:43:50 -0400

    Add column traits for other types

--- include/sqleger/column_traits.hpp
+++ include/sqleger/column_traits.hpp
@@ -4,6 +4,12 @@
 #include <sqleger/null.hpp>
 #include <sqleger/value.hpp>
 
+#include <array>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+
 
 namespace sqleger {
 
@@ -41,6 +47,66 @@ struct column_traits<uzstring_view> {
   static inline uzstring_view from_value(value_ref v) noexcept;
 };
 
+template <typename T, typename Allocator>
+struct column_traits<std::vector<T, Allocator>> {
+  static inline std::vector<T, Allocator> from_value(value_ref v) noexcept;
+};
+
+template <typename T, std::size_t N>
+struct column_traits<std::array<T, N>> {
+  static inline std::array<T, N> from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<float> {
+  static inline float from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<short> {
+  static inline short from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<unsigned short> {
+  static inline unsigned short from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<unsigned int> {
+  static inline unsigned int from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<long> {
+  static inline long from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<unsigned long> {
+  static inline unsigned long from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<bool> {
+  static inline bool from_value(value_ref v) noexcept;
+};
+
+template <typename T>
+struct column_traits<std::optional<T>> {
+  static inline std::optional<T> from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<char> {
+  static inline char from_value(value_ref v) noexcept;
+};
+
+template <>
+struct column_traits<std::string> {
+  static inline std::string from_value(value_ref v) noexcept;
+};
+
 
 // =============================================================================
 
@@ -73,6 +139,78 @@ null_t column_traits<null_t>::from_value(value_ref) noexcept
 uzstring_view column_traits<uzstring_view>::from_value(value_ref v) noexcept
 {
   return v.text();
+}
+
+template <typename T, typename Allocator>
+std::vector<T, Allocator>
+column_traits<std::vector<T, Allocator>>::from_value(value_ref v) noexcept
+{
+  const auto data = v.blob();
+  const auto size = static_cast<std::size_t>(v.bytes()) / sizeof(T);
+
+  return data.as_vector<T, Allocator>(size);
+}
+
+template <typename T, std::size_t N>
+std::array<T, N>
+column_traits<std::array<T, N>>::from_value(value_ref v) noexcept
+{
+  return v.blob().as_array<T, N>();
+}
+
+float column_traits<float>::from_value(value_ref v) noexcept
+{
+  return static_cast<float>(v._double());
+}
+
+short column_traits<short>::from_value(value_ref v) noexcept
+{
+  return static_cast<short>(v._int());
+}
+
+unsigned short column_traits<unsigned short>::from_value(value_ref v) noexcept
+{
+  return static_cast<unsigned short>(v._int());
+}
+
+unsigned int column_traits<unsigned int>::from_value(value_ref v) noexcept
+{
+  return static_cast<unsigned int>(v.int64());
+}
+
+long column_traits<long>::from_value(value_ref v) noexcept
+{
+  return static_cast<long>(v.int64());
+}
+
+unsigned long column_traits<unsigned long>::from_value(value_ref v) noexcept
+{
+  return static_cast<unsigned long>(v.int64());
+}
+
+bool column_traits<bool>::from_value(value_ref v) noexcept
+{
+  return static_cast<bool>(v._int());
+}
+
+template <typename T>
+std::optional<T>
+column_traits<std::optional<T>>::from_value(value_ref v) noexcept
+{
+  if (v.type() == datatype::null)
+    return std::nullopt;
+
+  return column_traits<T>::from_value(v);
+}
+
+char column_traits<char>::from_value(value_ref v) noexcept
+{
+  return static_cast<char>(*v.text().c_str());
+}
+
+std::string column_traits<std::string>::from_value(value_ref v) noexcept
+{
+  return utf8_to_ascii(v.text());
 }
 
 
--- src/sqleger/column_traits.test.cpp
+++ src/sqleger/column_traits.test.cpp
@@ -11,6 +11,8 @@
 using namespace sqleger;
 using namespace sqleger::string_span_literals;
 
+using namespace std::string_literals;
+
 
 TEST_CASE(
   "Column traits can be used to retrieve values through a universal interface",
@@ -172,6 +174,357 @@ TEST_CASE(
       = column_traits<uzstring_view>::from_value(s3.column_value(0));
     const auto stds = utf8_to_ascii(uzv);
     REQUIRE(string_span(stds) == ss);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("vector")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x BLOB NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto v1 = std::vector<uint64_t>({4, 3, 2, 1});
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = ::sqleger::bind(s2, v1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto v2
+      = column_traits<std::vector<uint64_t>>::from_value(s3.column_value(0));
+    REQUIRE(v2 == v1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("array")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x BLOB NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const std::array<uint64_t, 3> a1 {5, 2, 3};
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = ::sqleger::bind(s2, a1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto a2
+      = column_traits<std::array<uint64_t, 3>>::from_value(s3.column_value(0));
+    REQUIRE(a2 == a1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("float")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x REAL NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto float1 = 2.0f;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, float1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto float2 = column_traits<float>::from_value(s3.column_value(0));
+    REQUIRE(float2 == float1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("short")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto short1 = static_cast<short>(-31);
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, short1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto short2 = column_traits<short>::from_value(s3.column_value(0));
+    REQUIRE(short2 == short1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("unsigned short")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto ushort1 = static_cast<unsigned short>(789);
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, ushort1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto ushort2
+      = column_traits<unsigned short>::from_value(s3.column_value(0));
+    REQUIRE(ushort2 == ushort1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("unsigned int")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto uint1
+      = static_cast<unsigned int>(std::numeric_limits<int>::max()) + 1U;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, uint1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto uint2
+      = column_traits<unsigned int>::from_value(s3.column_value(0));
+    REQUIRE(uint2 == uint1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("long")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto long1 = static_cast<long>(std::numeric_limits<int>::max()) + 1L;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, long1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto long2 = column_traits<long>::from_value(s3.column_value(0));
+    REQUIRE(long2 == long1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("unsigned long")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto ulong1
+      = static_cast<unsigned long>(std::numeric_limits<int>::max()) + 1UL;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, ulong1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto ulong2
+      = column_traits<unsigned long>::from_value(s3.column_value(0));
+    REQUIRE(ulong2 == ulong1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("bool")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x INTEGER NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto bool1 = true;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, bool1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto bool2 = column_traits<bool>::from_value(s3.column_value(0));
+    REQUIRE(bool2 == bool1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("optional")
+  {
+    SECTION("null")
+    {
+      auto d = db(":memory:");
+
+      auto s1 = stmt(d, "CREATE TABLE t(x INTEGER)"_ss);
+      REQUIRE(s1.step() == result::done);
+
+      const auto optional1 = std::optional<bool>();
+
+      auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+      const auto [r, idx] = ::sqleger::bind(s2, optional1);
+      REQUIRE(r == result::ok);
+      REQUIRE(idx == 1);
+
+      REQUIRE(s2.step() == result::done);
+
+      auto s3 = stmt(d, "SELECT x FROM t"_ss);
+      REQUIRE(s3.step() == result::row);
+
+      const auto optional2
+        = column_traits<std::optional<bool>>::from_value(s3.column_value(0));
+      REQUIRE(optional2 == optional1);
+
+      REQUIRE(s3.step() == result::done);
+    }
+
+    SECTION("not null")
+    {
+      auto d = db(":memory:");
+
+      auto s1 = stmt(d, "CREATE TABLE t(x INTEGER)"_ss);
+      REQUIRE(s1.step() == result::done);
+
+      const auto optional1 = std::make_optional(true);
+
+      auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+      const auto [r, idx] = ::sqleger::bind(s2, optional1);
+      REQUIRE(r == result::ok);
+      REQUIRE(idx == 1);
+
+      REQUIRE(s2.step() == result::done);
+
+      auto s3 = stmt(d, "SELECT x FROM t"_ss);
+      REQUIRE(s3.step() == result::row);
+
+      const auto optional2
+        = column_traits<std::optional<bool>>::from_value(s3.column_value(0));
+      REQUIRE(optional2 == optional1);
+
+      REQUIRE(s3.step() == result::done);
+    }
+  }
+
+  SECTION("char")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x TEXT NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto char1 = '!';
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = bind(s2, char1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto char2 = column_traits<char>::from_value(s3.column_value(0));
+    REQUIRE(char2 == char1);
+
+    REQUIRE(s3.step() == result::done);
+  }
+
+  SECTION("std::string")
+  {
+    auto d = db(":memory:");
+
+    auto s1 = stmt(d, "CREATE TABLE t(x TEXT NOT NULL)"_ss);
+    REQUIRE(s1.step() == result::done);
+
+    const auto stds1 = "AJSCAPMOS Mdfaslflasfl#@$2323"s;
+
+    auto s2 = stmt(d, "INSERT INTO t VALUES(?1)"_ss);
+
+    const auto [r, idx] = ::sqleger::bind(s2, stds1);
+    REQUIRE(r == result::ok);
+    REQUIRE(idx == 1);
+
+    REQUIRE(s2.step() == result::done);
+
+    auto s3 = stmt(d, "SELECT x FROM t"_ss);
+    REQUIRE(s3.step() == result::row);
+
+    const auto stds2
+      = column_traits<std::string>::from_value(s3.column_value(0));
+    REQUIRE(stds2 == stds1);
 
     REQUIRE(s3.step() == result::done);
   }
