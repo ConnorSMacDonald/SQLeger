31ec8601e4a716074a836c28d6c3d6913c8a9a3c
csmacdonald95 <csmacdonald95@gmail.com>
2020-08-23 09:17:59 -0400

    Change bind traits to take parameters by value

--- include/sqleger/bind_traits.hpp
+++ include/sqleger/bind_traits.hpp
@@ -13,66 +13,65 @@ struct bind_traits;
 
 template <>
 struct bind_traits<user_blob> {
-  static inline result bind(parameter& p, const user_blob& value) noexcept;
+  static inline result bind(parameter p, const user_blob& value) noexcept;
 };
 
 template <>
 struct bind_traits<double> {
-  static constexpr int width = 1;
-  static inline result bind(parameter& p, double value) noexcept;
+  static inline result bind(parameter p, double value) noexcept;
 };
 
 template <>
 struct bind_traits<int> {
-  static inline result bind(parameter& p, int value) noexcept;
+  static inline result bind(parameter p, int value) noexcept;
 };
 
 template <>
 struct bind_traits<int64> {
-  static inline result bind(parameter& p, int64 value) noexcept;
+  static inline result bind(parameter p, int64 value) noexcept;
 };
 
 template <>
 struct bind_traits<null_t> {
-  static inline result bind(parameter& p, null_t) noexcept;
+  static inline result bind(parameter p, null_t) noexcept;
 };
 
 template <>
 struct bind_traits<user_text> {
-  static inline result bind(parameter& p, const user_text& value) noexcept;
+  static inline result bind(parameter p, const user_text& value) noexcept;
 };
 
 
 // =============================================================================
 
 
-result bind_traits<user_blob>::bind(parameter& p,
+result bind_traits<user_blob>::bind(parameter p,
                                     const user_blob& value) noexcept
 {
   return p.bind_blob(value);
 }
 
-result bind_traits<double>::bind(parameter& p, const double value) noexcept
+result bind_traits<double>::bind(parameter p, const double value) noexcept
 {
   return p.bind_double(value);
 }
 
-result bind_traits<int>::bind(parameter& p, const int value) noexcept
+result bind_traits<int>::bind(parameter p, const int value) noexcept
 {
   return p.bind_int(value);
 }
 
-result bind_traits<int64>::bind(parameter& p, const int64 value) noexcept
+result bind_traits<int64>::bind(parameter p, const int64 value) noexcept
 {
   return p.bind_int64(value);
 }
 
-result bind_traits<null_t>::bind(parameter& p, null_t) noexcept
+result bind_traits<null_t>::bind(parameter p, null_t) noexcept
 {
   return p.bind_null();
 }
 
-result bind_traits<user_text>::bind(parameter& p,
+result bind_traits<user_text>::bind(parameter p,
                                     const user_text& value) noexcept
 {
   return p.bind_text(value);
